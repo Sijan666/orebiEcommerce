44cc35faae82ff533f2bcf7ae50fd417c6e09ff6
Majharul Islam <666majharulislam@gmail.com>
2026-09-04 19:29:06 +0600

    refactor : update ui

--- src/components/layouts/Ads.jsx
+++ src/components/layouts/Ads.jsx
@@ -11,13 +11,13 @@ const Ads = () => {
         <div className="py-[50px] md:py-20 lg:py-[135px]">
             <Container>
                 <Flex className="flex-col lg:flex-row flex-wrap lg:flex-nowrap lg:justify-between lg:gap-x-8 gap-y-4 sm:gap-y-6 md:gap-y-8 lg:gap-y-0">
-                    {/* Left Banner */}
+                    {/* left banner */}
                     <div className="lg:leftSide w-full lg:w-1/2 relative flex justify-center">
                         <Link to={'/shop'} className="w-full" aria-label="Shop our pendant lamps sale">
                             <Images imgSrc={sh1} alt="Pendant lamps sale" />
                         </Link>
                     </div>
-                    {/* Right Banners */}
+                    {/* right banners */}
                     <div className="lg:rightSide w-full lg:w-1/2 relative">
                         <div className="lg:pb-8 w-full flex justify-center">
                             <Link to={'/shop'} className="w-full" aria-label="Shop electronics and clocks offer">
